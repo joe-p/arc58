3ecdb829a846ea4d8a57cce9dbc93972d32c8444
krby.algo <krby@akita.community>
2024-11-05 10:59:25 -0800

    feat: removed controlled account feature

--- __test__/permissions.test.ts
+++ __test__/permissions.test.ts
@@ -62,8 +62,6 @@ describe('ARC58 Plugin Permissions', () => {
 
     // Create an abstracted account app
     await abstractedAccountClient.create.createApplication({
-      // Set address to ZERO_ADDRESS so the app address is used
-      controlledAddress: ZERO_ADDRESS,
       // aliceEOA will be the admin
       admin: aliceEOA.addr,
     });
