83afd7c5077cc1fca07626611ae56d9edd1bc6ad
krby.algo <krby@akita.community>
2025-04-05 23:11:26 +0200

    fix: bug in rekeyTo rekeyback check that didnt account for controlled address

--- contracts/abstracted_account.algo.ts
+++ contracts/abstracted_account.algo.ts
@@ -126,7 +126,7 @@ export class AbstractedAccount extends Contract {
   }
 
   private txnRekeysBack(txn: gtxn.Transaction): boolean {
-    if (txn.sender === Global.currentApplicationAddress && txn.rekeyTo === Global.currentApplicationAddress) {
+    if (txn.sender === this.controlledAddress.value && txn.rekeyTo === Global.currentApplicationAddress) {
       return true;
     }
 
